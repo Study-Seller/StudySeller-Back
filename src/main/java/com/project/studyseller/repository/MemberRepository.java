87e931fd9b059ef19cad6bd0840851bf155066d0
khbbbbi <gkaqk0626@naver.com>
2023-05-18 23:35:05 +0900

    notionId 데이터-list로 리턴

--- src/main/java/com/project/studyseller/repository/MemberRepository.java
+++ src/main/java/com/project/studyseller/repository/MemberRepository.java
@@ -5,6 +5,7 @@ package com.project.studyseller.repository;
 import com.project.studyseller.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
